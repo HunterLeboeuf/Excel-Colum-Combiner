dfa9f0fac055a55f3951ec387ff92637d28b5039
Hunter Leboeuf <hjamesleboeuf@gmail.com>
2023-05-31 19:41:55 -0400

    Update Columncombiner.py
    changed wording on comments and fixed grammar errors

--- Columncombiner.py
+++ Columncombiner.py
@@ -1,24 +1,24 @@
 import pandas as pd
 from openpyxl import load_workbook
 
-
+# Change the file path to the Excel file you are working with 
 df = pd.read_excel("C:\\Users\\Hunter\\Desktop\\test.xlsx")
 
 
-# List's to grab each column 
+# List's that we will set each of our columns too 
 list1 = [ ]
 list2 = [ ]
 list3 = [ ]
 
-# Gather the data using pandas 
+# Gather the data using pandas, change t1 and t2 to what ever the title of the column is  
 list1 = df.loc[:, "t1"]
 list2 = df.loc[:, "t2"]
 
-# Combine both column 1 and column 2 for column 3 using extend 
+# Add both column 1 and column 2 for column 3 using extend 
 list3.extend(list1)
 list3.extend(list2)
 
-# Loads the workbook to print list 3 to 
+# Loads the workbook to print list 3 to change to the Excel you are working in 
 wb = load_workbook('C:\\Users\\Hunter\\Desktop\\test.xlsx')
 ws = wb.active
 
@@ -29,5 +29,5 @@ for x in list3:
     ws.cell(column=3, row=v, value=x)
     v = v + 1
     
-# Save the workbook change to your xlsx file path     
+# Save the workbook change to your .xlsx file path     
 wb.save('C:\\Users\\Hunter\\Desktop\\test.xlsx')
